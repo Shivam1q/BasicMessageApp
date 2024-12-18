5ef5a931d11fbaee083c732daf72a04a01a5dd44
Shivam1q <shivamyaaadaav@gmail.com>
2024-12-19 00:51:05 +0530

    Add the /new router of get and post and add form.ejs and this completes the project

--- app.js
+++ app.js
@@ -7,15 +7,24 @@ const PORT = 3000;
 app.set("view engine", "ejs");
 app.set("views", "views");
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', ( req, res ) => {
   res.render("index", {title: "Mini Messageboard", messages: messages});
 });
 
 app.get('/new', (req, res) => {
+  res.render("form", {title: "New Message"});
+})
 
+app.post('/new', (req, res) => {
+  const messageUser = req.body.fname;
+  const messageText = req.body.fmessage;
+
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+
+  res.redirect("/");
 });
-
-
 
 app.listen(PORT, () => {
   console.log("Express app is running on ", PORT);
